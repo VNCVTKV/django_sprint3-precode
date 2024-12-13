522d0eb4413cd0b6e61c677d8e7097aeaf8614be
Ivan Tcvetkov <cvetkoviv@yandex.ru>
2024-12-13 23:02:22 +0300

    complete admin page, doing view function

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+from .models import Post, Category
+from datetime import datetime
 
 
 def index(request):
     template = 'blog/index.html'
-    reversed_posts = posts[::-1]
-    context = {'posts': reversed_posts}
+    posts = Post.objects.values(
+        'id', 'pub_date', 'location', 'text', 
+        'location__is_published', 'location__name',
+        'author__username',
+        'category__title', 'category__slug'
+    ).filter(
+        is_published=True,
+        category__is_published=True,
+        pub_date__lte=datetime.now()
+    ).order_by('-pub_date')[:5] 
+    context = {'post_list': posts}
     return render(request, template, context)
 
 
@@ -17,5 +28,23 @@ def post_detail(request, id):
 
 def category_posts(request, category_slug):
     template = 'blog/category.html'
-    context = {'category': category_slug}
+    category_ = get_object_or_404(
+        Category.objects.values(
+            'title', 'description'
+        ).filter(is_published=True),
+        slug=category_slug
+    )
+    posts = Post.objects.values(
+        'id', 'pub_date', 'location', 'text', 
+        'location__is_published', 'location__name',
+        'author__username',
+        'category__title', 'category__slug'
+    ).filter(
+        is_published=True,
+        category__is_published=True,
+        pub_date__lte=datetime.now(),
+        category__slug=category_slug
+    ).order_by('-pub_date')
+    context = {'category': category_,
+               'post_list': posts}
     return render(request, template, context)
